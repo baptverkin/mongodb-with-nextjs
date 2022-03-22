273e5e4b8f98491a05192eb19cd794239e1f582f
Baptiste Verkindere <bverkindere@gmail.com>
2022-03-22 17:45:58 +0100

    adding files for connexion to MongoDB with Nex JS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,32 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { getDatabase } from '../database'
+import { GetServerSideProps } from 'next'
 
-const Home: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const mongoDb = await getDatabase();
+  const getDirector = await mongoDb.db().collection("directors").find().toArray();
+  const director = await JSON.parse(JSON.stringify(getDirector))
+  console.log("test2", {director})
+
+  return {
+    props : {
+      director: director,
+    }
+  }
+}
+
+type MyRectComponent={
+  director: {
+    _id: string,
+    firstName: string,
+    lastName: string,
+  }[]
+}
+
+const Home: NextPage<MyRectComponent> = ({director}) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,57 +38,15 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {director.map((firstNames, index) => {
+            return (
+              <h1 className={styles.title} key={index}>
+              Welcome to {firstNames.firstName}
+              </h1>
+              )
+              })}
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
